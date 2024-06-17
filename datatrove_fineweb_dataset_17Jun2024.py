650df2a890d6cb79484fa66c7560a7053efd2015
Mr. Jack Tung <72214141+Mr-Jack-Tung@users.noreply.github.com>
2024-06-17 19:31:49 +0700

    Update datatrove_fineweb_dataset_17Jun2024.py

--- datatrove_fineweb_dataset_17Jun2024.py
+++ datatrove_fineweb_dataset_17Jun2024.py
@@ -15,6 +15,11 @@ from datatrove.pipeline.readers import ParquetReader
 # limit determines how many documents will be streamed (remove for all)
 # to fetch a specific dump: hf://datasets/HuggingFaceFW/fineweb/data/CC-MAIN-2024-10
 # replace "data" with "sample/100BT" to use the 100BT sample
+
+# data_file = 'data/CC-MAIN-2024-10'
+# data_file = 'sample/100BT'
+# data_reader = ParquetReader("hf://datasets/HuggingFaceFW/fineweb/"+data_file, limit=3) 
+
 data_reader = ParquetReader("hf://datasets/HuggingFaceFW/fineweb/data", limit=3) 
 for document in data_reader():
     # do something with document
