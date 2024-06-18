a5924c951096f9397407e75cbf7d7b1f3e92cf6e
Mr. Jack Tung <72214141+Mr-Jack-Tung@users.noreply.github.com>
2024-06-18 12:24:40 +0700

    Update huggingface_hub_list_datasets_164k_17Jun2024.py

--- huggingface_hub_list_datasets_164k_17Jun2024.py
+++ huggingface_hub_list_datasets_164k_17Jun2024.py
@@ -4,18 +4,18 @@ File: huggingface_hub_list_datasets_164k_17Jun2024.py
 - From: Mr.Jack
 - Date: 17 Jun 2024
 """
-
+# % pip install datasets
 from datasets import list_datasets
 
 huggingface_hub_list_datasets = list_datasets()
 
 print("\n","huggingface_hub_list_datasets:",len(huggingface_hub_list_datasets),"datasets")
 
-# (17/06) huggingface_hub_list_datasets: 163.963 datasets
-# (18/06) huggingface_hub_list_datasets: 164.588 datasets
-
 # for dataset_path in huggingface_hub_list_datasets:
 #     print(dataset_path)
+
+# (17/06) huggingface_hub_list_datasets: 163.963 datasets
+# (18/06) huggingface_hub_list_datasets: 164.588 datasets
 
 for dataset_path in huggingface_hub_list_datasets[:5]:
     print("\n","="*60)
